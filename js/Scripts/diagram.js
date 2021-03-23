b4462ca54e4e8a9f09571d395a72e425ee724e23
Hans434 <hansheje@hotmail.com>
2021-03-23 13:36:56 +0100

    asd

--- js/Scripts/diagram.js
+++ js/Scripts/diagram.js
@@ -1,28 +1,28 @@
-let expC = 2;
-let colorC = "rgb(0,0,255)";
-let expHTML = 1;
-let colorHTML = "rgb(255, 153, 0)";
-let expCSS = 1; 
-let colorCSS = "rgb(0, 153, 255)";
-let expJS = 1;
-let colorJS = "rgb(255, 255, 0)";
-let expCsharp = 1;
-let colorCsharp = "rgb(0, 153, 51)";
+let expFrontEnd = 1;
+let colorFrontEnd = "rgb(255, 255, 0)";
+let expBackEnd = 0;
+let colorBackEnd = "rgb(255, 153, 0)";
 let expElectronics = 2;
 let colorElectronics = "rgb(0, 255, 0)"
 let expJava = 1;
 let colorJava = "rgb(255, 51, 0)";
+let expGameDev = 1;
+let colorGameDev = "rgb(70, 70, 120)";
+let expC = 2;
+let colorC = "rgb(0,0,255)";
+let expGroup = 1;
+let colorGroup = "rgb(255,0,0)";
 
 var ctx = document.getElementById('pichart').getContext('2d');
 new Chart(document.getElementById("pichart"),
   {
     "type": "doughnut", "data": {
-      "labels": ["C","C#","HTML", "CSS", "JavaScript","Electronics","Java"],
+      "labels": ["Front End","Back End","Electronics", "Game Dev.","C","Large Groups (>4)"],
       "datasets": [{
         //"borderWidth": [20],
         "label": "Project Categories", 
-        "data": [expC,expCsharp,expHTML,expCSS,expJS,expElectronics,expJava],
-        "backgroundColor": [colorC,colorCsharp,colorHTML,colorCSS,colorJS,colorElectronics,colorJava]
+        "data": [expFrontEnd,expBackEnd,expElectronics,expGameDev,expC,expGroup],
+        "backgroundColor": [colorFrontEnd,colorBackEnd,colorElectronics,colorGameDev,colorC,colorGroup]
         //"hidden": true;
       }]
     }
